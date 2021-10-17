f269fc40ea1350917063649c6b72eedb760c1e6f
Neng Shi <shineng@zju.edu.cn>
2021-10-17 10:41:22 -0400

    lab4: the first de Bohr algorithm

--- lab4/bsplinesurf.py
+++ lab4/bsplinesurf.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
+
 # load vX and vY from a textfile
 def loadTextFile(filename):
     vX, vY = [], []
@@ -21,71 +22,71 @@ def loadTextFile(filename):
 
     assert (numv == len(vX))
 
-    # plt.plot(vX, vY, color="magenta")
-    # plt.plot(vX, vY, 'ob', picker=True, pickradius=5)
-    k = ndegree + 1
+    return ndegree, knots, np.array(vX), np.array(vY)
 
-    pX, pY = np.array([]), np.array([])
-    for j in range(k - 1, numv):
-        q = np.zeros((k, 50, 2))
-        u = np.linspace(0, 1, 50) * (knots[j + 1] - knots[j]) + knots[j]
-        for i in range(j - k + 1, j + 1):
-            q[i - (j - k + 1), :, 0] = vX[i]
-            q[i - (j - k + 1), :, 1] = vY[i]
-        for r in range(1, k):
-            h = k - r
-            # construct control points for order h spline
-            for i in range(j, j - h, -1):
-                alpha = (u - knots[i]) / (knots[i + h] - knots[i])
-                q[i - (j - k + 1)] = (1.0 - alpha)[:, np.newaxis] * q[i - 1 - (j - k + 1)] + alpha[:, np.newaxis] * q[i - (j - k + 1)]
-        pX = np.concatenate((pX, q[k - 1, :, 0]))
-        pY = np.concatenate((pY, q[k - 1, :, 1]))
-    # plt.plot(pX, pY, 'b-')
-    # plt.draw()
-    # plt.show()
-    return pX, pY
 
 def sweepbspline(args):
     # log parameters
     print(args)
 
-    infile1Y, infile1Z = loadTextFile(args.infile1)
-    infile1X = np.zeros_like(infile1Y)
-    alpha = np.hstack((infile1X[:, np.newaxis], infile1Y[:, np.newaxis], infile1Z[:, np.newaxis]))
-    num_alpha_points = alpha.shape[0]
-    infile2X, infile2Y = loadTextFile(args.infile2)
-    infile2Z = np.zeros_like(infile2X)
-    beta = np.hstack((infile2X[:, np.newaxis], infile2Y[:, np.newaxis], infile2Z[:, np.newaxis]))
-    beta = beta - beta[0]
-    num_beta_points = beta.shape[0]
+    d1, knots1, y1, z1 = loadTextFile(args.infile1)
+    x1 = np.zeros_like(y1)
+    q1 = np.hstack((x1[:, np.newaxis], y1[:, np.newaxis], z1[:, np.newaxis]))
+    numv1 = q1.shape[0]
+    k1 = d1 + 1
 
-    alpha = np.tile(alpha[:, np.newaxis, :], (1, num_beta_points, 1))
-    beta = np.tile(beta[np.newaxis, :, :], (num_alpha_points, 1, 1))
+    d2, knots2, x2, y2 = loadTextFile(args.infile2)
+    z2 = np.zeros_like(x2)
+    q2 = np.hstack((x2[:, np.newaxis], y2[:, np.newaxis], z2[:, np.newaxis]))
+    q2 = q2 - q2[0]
+    numv2 = q2.shape[0]
+    k2 = d2 + 1
 
-    q = alpha + beta
+    q1 = np.tile(q1[:, np.newaxis, :], (1, numv2, 1))
+    q2 = np.tile(q2[np.newaxis, :, :], (numv1, 1, 1))
+    q = q1 + q2  # [numv1, numv2]
+
+    for j in range(k2 - 1, numv2):
+        q_tmp = np.zeros((numv1, k2, 50, 3))
+        u = np.linspace(0, 1, 50) * (knots2[j + 1] - knots2[j]) + knots2[j]
+        for i in range(j - k2 + 1, j + 1):
+            q_tmp[:, i - (j - k2 + 1), :, :] = q[:, i:i+1, :]
+        for r in range(1, k2):
+            h = k2 - r
+            for i in range(j, j - h, -1):
+                alpha = (u - knots2[i]) / (knots2[i + h] - knots2[i])
+                q_tmp[:, i - (j - k2 + 1)] = (1.0 - alpha)[np.newaxis, :, np.newaxis] * q_tmp[:, i - 1 - (j - k2 + 1)] + \
+                                             alpha[np.newaxis, :, np.newaxis] * q_tmp[:, i - (j - k2 + 1)]
+        if j == k2 - 1:
+            q_hat = q_tmp[:, k2 - 1, :, :]
+        else:
+            q_hat = np.concatenate((q_hat, q_tmp[:, k2 - 1, :, :]), axis=1)
+
+
 
     f = open(args.outfile, "w")
-    f.write("OFF\n{:d} {:d} 0\n".format(num_alpha_points * num_beta_points, (num_alpha_points - 1) * (num_beta_points - 1)))
+    f.write("OFF\n{:d} {:d} 0\n".format(numv1 * numv2, (numv1 - 1) * (numv2 - 1)))
 
-    for i in range(num_alpha_points):
-        for j in range(num_beta_points):
+    for i in range(numv1):
+        for j in range(numv2):
             f.write("{:f} {:f} {:f}\n".format(q[i, j, 0], q[i, j, 1], q[i, j, 2]))
 
-    for i in range(num_alpha_points - 1):
-        for j in range(num_beta_points - 1):
+    for i in range(numv1 - 1):
+        for j in range(numv2 - 1):
             f.write("4  {:d} {:d} {:d} {:d}\n".format(
-                i * num_beta_points + j, (i+1) * num_beta_points + j,
-                (i+1) * num_beta_points + j+1, i * num_beta_points + j+1))
+                i * numv2 + j, (i + 1) * numv2 + j,
+                (i + 1) * numv2 + j + 1, i * numv2 + j + 1))
 
     return
 
+
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument("--infile1", required=True, type=str,
-                        help="the first B-spline curve")
+                    help="the first B-spline curve")
 parser.add_argument("--infile2", required=True, type=str,
-                        help="the second B-spline curve")
+                    help="the second B-spline curve")
 parser.add_argument("--outfile", default="out.off", type=str,
-                        help="the name of the output file")
+                    help="the name of the output file")
 
 if __name__ == '__main__':
     args = parser.parse_args()
